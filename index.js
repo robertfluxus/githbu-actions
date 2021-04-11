0f3f49ccdae924d5a4742f24d00f886af2ca3a45
Robert <rsmarincu@gmail.com>
2021-04-11 15:59:42 +0100

    Added action

---
+++ index.js
@@ -0,0 +1,74 @@
+const core = require("@actions/core");
+const github = require("@actions/github");
+const axios = require("axios");
+
+const trelloKey = core.getInput("trello-key", { required: true });
+const trelloToken = core.getInput("trello-token", { required: true });
+const boardID = core.getInput("board-id", { required: true });
+const listID = core.getInput("list-id", { required: true });
+
+const trelloClient = axios.create({
+  baseUrl: "https://api.trello.com",
+});
+
+const requestTrello = async (verb, url, body = null, extraParams = null) => {
+  try {
+    const params = {
+      ...(extraParams || {}),
+      key: trelloKey,
+      token: trelloToken,
+    };
+
+    const res = await trelloClient.request({
+      method: verb,
+      url: url,
+      data: body || {},
+      params: params,
+    });
+    core.debug(
+      `${verb} to ${url} completed with status: ${res.status}.  data follows:`
+    );
+    //BRH NOTE core.xxx logging methods explode with typeerror when given non-string object.  TODO wrap.
+    core.debug(util.inspect(res.data));
+    return res.data;
+  } catch (err) {
+    core.error(`${verb} to ${url} errored: ${err}`);
+    if (err.response) {
+      core.error(util.inspect(err.response.data));
+    }
+    throw err;
+  }
+};
+
+const getCard = async (branchName) => {
+  return requestTrello("get", `/1/search`, {
+    query: branchName,
+    idBoards: [boardID],
+    partial: true,
+  });
+};
+
+const moveCard = async (cardID, listID) => {
+  return requestTrello("put", `/1/cards/${cardID}`, {
+    idList: listID,
+  });
+};
+
+let branchName = github.ref;
+branchName = branchName.split("/")[branchName.length - 1];
+
+(async () => {
+  try {
+    if (!(github.context.eventName === "create")) {
+      core.info(`event not supported, skipping action.`);
+      return;
+    }
+
+    card = await getCard(branchName);
+    await moveCard(card.id, listID);
+  } catch (error) {
+    core.error(util.inspect(error));
+    //failure will stop PR from being mergeable if that setting enabled on the repo.  there is not currently a neutral exit in actions v2.
+    core.setFailed(error.message);
+  }
+})();
